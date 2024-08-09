e9db915b2b2aa479c4f4f7f197ecc68b2c766d23
dinosnake666 <ahamada0506@gmail.com>
2024-08-09 13:31:32 +0100

    comments added

--- ft_atoi.c
+++ ft_atoi.c
@@ -9,23 +9,25 @@ int	ft_atoi(char *str)
 	sign_count = 0;
 	num = 0;
 	symb = 0;
+
 	while (str[index] == ' ' || str[index] == '\n'
-			|| str[index] == '\v' || str[index] == '\t'
+			|| str[index] == '\v' || str[index] == '\t'  //ignoring the whitespaces (i know, i hate that too)
 			|| str[index] == '\r'|| str[index] == '\f')
 		index++;
-	while (str[index] == '+' || str[index] == '-')
+
+	while (str[index] == '+' || str[index] == '-') //yay, let's do nothing with '+'
 	{
 		if (str[index] == '-')
 			sign_count++;
 		index++;
-		symb++;
+		symb++; //you can delete symb if you want the quirky atoi
 	}
 	while ('0' <= str[index] && str[index]<= '9')
 	{
-		num = (num * 10) + (str[index] - '0');
+		num = (num * 10) + (str[index] - '0'); //converting process 
 		index++;
 	}
-	if ((sign_count % 2 == 0) && symb <= 1)
+	if ((sign_count % 2 == 0) && symb <= 1) //symb is just there to be annoying (be exactly like atoi)
 		return(num);
 	else if (symb <= 1)
 		return(num * -1);

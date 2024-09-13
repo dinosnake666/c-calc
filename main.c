207b04382b93ee01e6795b0ed0f38630528edcd3
dinosnake666 <158581931+dinosnake666@users.noreply.github.com>
2024-09-13 19:58:22 +0200

    re-org

--- main.c
+++ main.c
@@ -4,14 +4,3 @@
 void	ft_putnbr(int n);
 int	ft_atoi(char *str);
 void	ft_putstr(char *str);
-
-int	main(int argc, char **argv)
-{
-	if (argc == 4)
-	{
-
-	}
-	else
-		ft_putstr("\aError : format not respected\tFormat : NUM1 OPERAND NUM2\n");
-	retrun (0);
-}
